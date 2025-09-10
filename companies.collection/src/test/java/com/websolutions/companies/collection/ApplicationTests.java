23300ec5a03b52ac54cb219d1a71debacfd9956f
mouadtouhafi <touhafimouad@gmail.com>
2025-09-10 13:33:07 +0100

    Initial commit

---
+++ companies.collection/src/test/java/com/websolutions/companies/collection/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websolutions.companies.collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
