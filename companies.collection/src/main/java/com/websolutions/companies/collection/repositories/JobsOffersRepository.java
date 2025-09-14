ac34795071d95cbc9664659c4476d74e09ddffd2
mouadtouhafi <touhafimouad@gmail.com>
2025-09-15 00:31:07 +0100

    Cleaning file.

--- companies.collection/src/main/java/com/websolutions/companies/collection/repositories/JobsOffersRepository.java
+++ companies.collection/src/main/java/com/websolutions/companies/collection/repositories/JobsOffersRepository.java
@@ -1,9 +1,11 @@
 package com.websolutions.companies.collection.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.websolutions.companies.collection.entites.JobsOffers;
 
+@Repository
 public interface JobsOffersRepository extends JpaRepository<JobsOffers, Long>{
 	
 	boolean existsByTitleAndCompanyAndLocationAndUrl(String title, String company, String location, String url);
