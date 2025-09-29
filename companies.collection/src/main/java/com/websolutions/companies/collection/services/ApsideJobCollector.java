01f749b5bceb7743a0d7f041b4f34066956936e9
mouadtouhafi <touhafimouad@gmail.com>
2025-09-29 21:18:12 +0100

    Initial commit

---
+++ companies.collection/src/main/java/com/websolutions/companies/collection/services/ApsideJobCollector.java
@@ -0,0 +1,32 @@
+package com.websolutions.companies.collection.services;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import com.websolutions.companies.collection.entites.JobsOffers;
+import com.websolutions.companies.collection.repositories.JobsOffersRepository;
+
+@Service
+public class ApsideJobCollector {
+	
+	
+}
