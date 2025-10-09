5660f05946b2274b62b423e1a30939a25b042a67
mouadtouhafi <touhafimouad@gmail.com>
2025-10-10 00:27:46 +0100

    Adding the window size argument.

--- companies.collection/src/main/java/com/websolutions/companies/collection/services/SqliJobCollector.java
+++ companies.collection/src/main/java/com/websolutions/companies/collection/services/SqliJobCollector.java
@@ -51,6 +51,7 @@ public class SqliJobCollector {
 		options.addArguments("--lang=en-US");
         options.addArguments("--disable-gpu");
         options.addArguments("--disable-notifications");
+        options.addArguments("--window-size=1920,1080");
 		
         driver = new RemoteWebDriver(
         		URI.create("http://selenium:4444").toURL(),
