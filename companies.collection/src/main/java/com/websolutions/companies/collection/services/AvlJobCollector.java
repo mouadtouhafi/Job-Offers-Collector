f7c51a8f40eb936a834c3f39b5e9ab30c7709e36
mouadtouhafi <touhafimouad@gmail.com>
2025-10-10 00:22:12 +0100

    Adding the window size argument.

--- companies.collection/src/main/java/com/websolutions/companies/collection/services/AvlJobCollector.java
+++ companies.collection/src/main/java/com/websolutions/companies/collection/services/AvlJobCollector.java
@@ -54,6 +54,7 @@ public class AvlJobCollector {
 		options.addArguments("--lang=en-US");
         options.addArguments("--disable-gpu");
         options.addArguments("--disable-notifications");
+        options.addArguments("--window-size=1920,1080");
 		
         driver = new RemoteWebDriver(
         		URI.create("http://selenium:4444").toURL(),
