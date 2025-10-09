d550e651d2798b2da12cd3b08860b3bf6b3fe5dd
mouadtouhafi <touhafimouad@gmail.com>
2025-10-10 00:23:10 +0100

    Adding the size window argument.

--- companies.collection/src/main/java/com/websolutions/companies/collection/services/DevoteamJobCollector.java
+++ companies.collection/src/main/java/com/websolutions/companies/collection/services/DevoteamJobCollector.java
@@ -52,6 +52,7 @@ public class DevoteamJobCollector {
 		options.addArguments("--lang=en-US");
         options.addArguments("--disable-gpu");
         options.addArguments("--disable-notifications");
+        options.addArguments("--window-size=1920,1080");
 		
         driver = new RemoteWebDriver(
         		URI.create("http://selenium:4444").toURL(),
