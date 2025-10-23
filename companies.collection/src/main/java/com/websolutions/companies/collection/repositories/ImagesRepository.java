82f4107ea2fa8510a39eefdab58c2e02055d2d07
mouadtouhafi <touhafimouad@gmail.com>
2025-10-23 22:14:39 +0100

    Initial commit.

---
+++ companies.collection/src/main/java/com/websolutions/companies/collection/repositories/ImagesRepository.java
@@ -0,0 +1,9 @@
+package com.websolutions.companies.collection.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.websolutions.companies.collection.entites.ImagesEntity;
+
+public interface ImagesRepository extends JpaRepository<ImagesEntity, Long>{
+	
+}
