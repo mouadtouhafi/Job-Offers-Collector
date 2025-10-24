e7390772bc1957cfcb7df955d72cf293097d85e9
mouadtouhafi <touhafimouad@gmail.com>
2025-10-24 14:21:16 +0100

    Adding saving method.

--- companies.collection/src/main/java/com/websolutions/companies/collection/services/repositories/ImportImagesService.java
+++ companies.collection/src/main/java/com/websolutions/companies/collection/services/repositories/ImportImagesService.java
@@ -1,7 +1,11 @@
 package com.websolutions.companies.collection.services.repositories;
 
 import org.springframework.stereotype.Service;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
+import com.websolutions.companies.collection.entites.ImagesEntity;
 import com.websolutions.companies.collection.repositories.ImagesRepository;
 
 @Service
@@ -13,5 +17,22 @@ public class ImportImagesService {
 		this.imagesRepository = imagesRepository;
 	}
 	
+	public void loadImages(String folderPath) throws IOException {
+		File folder = new File(folderPath);
+		if(!folder.exists() || !folder.isDirectory()) {
+			throw new IllegalArgumentException("Folder path is invalid : " + folderPath);
+		}
+		File[] files = folder.listFiles((dir, name) -> name.endsWith(".png") || name.endsWith(".jpg"));
+		if(files == null) {
+			return;
+		}
+		
+		for(File file : files) {
+			byte[] data = Files.readAllBytes(file.toPath());
+			String fileType = Files.probeContentType(file.toPath());
+			ImagesEntity image = new ImagesEntity(file.getName(), fileType, data);
+			imagesRepository.save(image);
+		}
+	}
 	
 }
