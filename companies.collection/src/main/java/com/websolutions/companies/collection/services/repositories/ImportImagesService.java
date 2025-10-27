e26637e88727198a0679509da65126ec0d27807e
mouadtouhafi <touhafimouad@gmail.com>
2025-10-27 16:14:09 +0100

    Adding method to load flags images

--- companies.collection/src/main/java/com/websolutions/companies/collection/services/repositories/ImportImagesService.java
+++ companies.collection/src/main/java/com/websolutions/companies/collection/services/repositories/ImportImagesService.java
@@ -7,7 +7,9 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Service;
 
+import com.websolutions.companies.collection.entites.FlagsEntity;
 import com.websolutions.companies.collection.entites.ImagesEntity;
+import com.websolutions.companies.collection.repositories.FlagsRepository;
 import com.websolutions.companies.collection.repositories.ImagesRepository;
 
 
@@ -16,9 +18,11 @@ import com.websolutions.companies.collection.repositories.ImagesRepository;
 public class ImportImagesService {
 	
 	private ImagesRepository imagesRepository;
+	private FlagsRepository flagsRepository;
 	
-	public ImportImagesService(ImagesRepository imagesRepository) {
+	public ImportImagesService(ImagesRepository imagesRepository, FlagsRepository flagsRepository) {
 		this.imagesRepository = imagesRepository;
+		this.flagsRepository = flagsRepository;
 	}
 	
 	public void loadImages(String folderPath) throws IOException {
@@ -37,8 +41,24 @@ public class ImportImagesService {
 				}
 			}
 		}
+	}
+	
+	public void loadFlags(String folderPath) throws IOException {
+		PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+		Resource[] resources = resolver.getResources("classpath:" + folderPath + "/*.{png,jpg,jpeg}");
 		
-		
+		for(Resource resource : resources) {
+			try (InputStream is = resource.getInputStream()){
+				byte[] data = is.readAllBytes();
+				String fileName = resource.getFilename().split("\\.")[0];
+				String fileType = fileName.endsWith(".png") ? "image/png" : "image/jpeg";
+				
+				FlagsEntity image = new FlagsEntity(fileName, fileType, data);
+				if(!flagsRepository.existsByName(fileName)) {
+					flagsRepository.save(image);
+				}
+			}
+		}
 	}
 	
 }
