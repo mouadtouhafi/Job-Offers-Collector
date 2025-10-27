03d429943b2a1b11cc3bd55d94a331c36e33537b
mouadtouhafi <touhafimouad@gmail.com>
2025-10-27 14:08:52 +0100

    Adding existsByName method

--- companies.collection/src/main/java/com/websolutions/companies/collection/repositories/ImagesRepository.java
+++ companies.collection/src/main/java/com/websolutions/companies/collection/repositories/ImagesRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.websolutions.companies.collection.entites.ImagesEntity;
 
 public interface ImagesRepository extends JpaRepository<ImagesEntity, Long>{
-	
+	boolean existsByName(String name);
 }
