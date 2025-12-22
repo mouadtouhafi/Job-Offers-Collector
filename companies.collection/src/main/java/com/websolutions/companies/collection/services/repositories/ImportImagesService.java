4b6dea44290d8e882e2369c92148f79172655e32
mouadtouhafi <touhafimouad@gmail.com>
2025-12-22 20:46:36 +0100

    Adding fieldsImagesRepository.

--- companies.collection/src/main/java/com/websolutions/companies/collection/services/repositories/ImportImagesService.java
+++ companies.collection/src/main/java/com/websolutions/companies/collection/services/repositories/ImportImagesService.java
@@ -7,8 +7,10 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Service;
 
+import com.websolutions.companies.collection.entites.FieldsImagesEntity;
 import com.websolutions.companies.collection.entites.FlagsEntity;
 import com.websolutions.companies.collection.entites.ImagesEntity;
+import com.websolutions.companies.collection.repositories.FieldsImagesRepository;
 import com.websolutions.companies.collection.repositories.FlagsRepository;
 import com.websolutions.companies.collection.repositories.ImagesRepository;
 
@@ -19,10 +21,12 @@ public class ImportImagesService {
 	
 	private ImagesRepository imagesRepository;
 	private FlagsRepository flagsRepository;
+	private FieldsImagesRepository fieldsImagesRepository;
 	
-	public ImportImagesService(ImagesRepository imagesRepository, FlagsRepository flagsRepository) {
+	public ImportImagesService(ImagesRepository imagesRepository, FlagsRepository flagsRepository, FieldsImagesRepository fieldsImagesRepository) {
 		this.imagesRepository = imagesRepository;
 		this.flagsRepository = flagsRepository;
+		this.fieldsImagesRepository = fieldsImagesRepository;
 	}
 	
 	public void loadImages(String folderPath) throws IOException {
@@ -56,6 +60,24 @@ public class ImportImagesService {
 				FlagsEntity image = new FlagsEntity(fileName, fileType, data);
 				if(!flagsRepository.existsByName(fileName)) {
 					flagsRepository.save(image);
+				}
+			}
+		}
+	}
+	
+	public void loadFieldsImages(String folderPath) throws IOException {
+		PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+		Resource[] resources = resolver.getResources("classpath:" + folderPath + "/*.{png,jpg,jpeg}");
+		
+		for(Resource resource : resources) {
+			try (InputStream is = resource.getInputStream()){
+				byte[] data = is.readAllBytes();
+				String fileName = resource.getFilename().split("\\.")[0];
+				String fileType = fileName.endsWith(".png") ? "image/png" : "image/jpeg";
+				
+				FieldsImagesEntity image = new FieldsImagesEntity(fileName, fileType, data);
+				if(!fieldsImagesRepository.existsByName(fileName)) {
+					fieldsImagesRepository.save(image);
 				}
 			}
 		}
